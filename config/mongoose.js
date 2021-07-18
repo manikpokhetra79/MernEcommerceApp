ce7756f4dcf58acd1ea6b34ecbc0b0401225fb52
Manik Gupta <manikpokhetra@gmail.com>
2021-07-18 12:55:32 +0530

    installed mongoose and created userSchema

---
+++ config/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ecom_app_db');
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"Database connection error:"));
+db.once('open',function(){
+    console.log("Successfully connected to databse");
+});
+module.exports = db;
