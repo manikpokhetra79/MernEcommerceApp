fcd387c31122ee8456e012eca2758919c7fc6c9e
Manik Gupta <manikpokhetra@gmail.com>
2021-07-18 12:46:07 +0530

    Installed sass middleware Added custom scss for homepage

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const port = 8000;
+const sassMiddleware = require('node-sass-middleware');
 const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
+// use sass middleware
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix : '/css'
+}));
 // use static files
 app.use(express.static('./assets'));
 // setup ejs
@@ -9,6 +19,9 @@ app.set('view engine','ejs');
 app.set('views','./views');
 // use expressLayouts
 app.use(expressLayouts);
+// extract scripts and stylesheets
+app.set("layout extractScripts",true);
+app.set("layout extractStyles",true);
 // route for homepage
 app.use('/',require('./routes/index'));
 app.listen(port,function(err){
